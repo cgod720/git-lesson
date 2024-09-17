9198ed41fe4d41072c67a3c1ff3ca5e407cccb16
Carlos Godoy <carlosgodoy@pursuit.org>
2024-09-17 15:37:15 -0400

    rebasing two merges

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const coolFunction = () => {
     return "you're the man."
 }
 
-
 const haduken = () => {
     return "HADUKEN!"
+}
+const anotherFunction = () => {
+    return "we're about to rock this project🔥"
 }
